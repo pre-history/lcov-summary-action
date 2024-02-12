190e7e5b55b499eedf332ee6e4a5947b0e8afcdb
Anas Boudih <metanas@pre-history.com>
2024-02-12 22:10:23 +0100

    feat: run lcov on action (#7)

--- nyc.config.js
+++ nyc.config.js
@@ -2,5 +2,7 @@ module.exports = {
   extends: '@istanbuljs/nyc-config-typescript',
   all: true,
   'check-coverage': true,
-  exclude: ['test/**/*.*', 'dist/**', '*.config.*'],
+  reporter: ['lcov', 'text'],
+  'skip-full': true,
+  exclude: ['test/**/*.*', 'dist/**', '*.config.*', 'coverage/**'],
 };
