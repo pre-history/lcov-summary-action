ddbc06251c3b3af8fab05be9fd055dbb92f22c2f
Abdelkader Boudih <terminale@gmail.com>
2024-09-27 14:19:33 +0100

    Update lcov_parser.ts

--- src/lcov_parser.ts
+++ src/lcov_parser.ts
@@ -12,15 +12,17 @@ export function parseLcov(lcov: string): ParseLcov {
 
   lcovLines.forEach((line) => {
     if (line.startsWith('LH:')) {
-      linesHit += parseInt(line.split(':')[1]);
+      const value = parseInt(line.split(':')[1], 10);
+      if (!isNaN(value)) linesHit += value;
     } else if (line.startsWith('LF:')) {
-      totalLines += parseInt(line.split(':')[1]);
+      const value = parseInt(line.split(':')[1], 10);
+      if (!isNaN(value)) totalLines += value;
     }
   });
 
   let coveragePercentage = 0;
   if (totalLines > 0) {
-    coveragePercentage = (linesHit / totalLines) * 100;
+    coveragePercentage = Number(((linesHit / totalLines) * 100).toFixed(2));
   }
 
   return {
